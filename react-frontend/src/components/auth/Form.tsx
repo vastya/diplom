1e4740da9ce601185360b73b967c5fd69ad09faf
Darya Shimanskaya <dshimanskaya@beamplify.com>
2023-06-23 00:25:21 +0300

    update url

--- react-frontend/src/components/auth/Form.tsx
+++ react-frontend/src/components/auth/Form.tsx
@@ -30,7 +30,7 @@ function Form(props: Props) {
     try {
       await onSubmit(form);
       toast(type === 'LOGIN' ? 'You have logged in!' : 'Your account is created!');
-      window.location.replace('http://localhost:5173/project');
+      window.location.replace('https://diplom-olive.vercel.app/project');
     } catch (error) {
       setError(((error as AxiosError).response?.data as APIERROR).message);
     }
